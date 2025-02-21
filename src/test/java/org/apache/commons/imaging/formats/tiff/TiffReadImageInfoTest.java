fcdf4c194c9bec8ff963e1d942d879567cb4ec13
hugolw <hugolw@kth.se>
2025-02-21 12:00:33 +0100

    added some tests. Relates to #20

--- src/test/java/org/apache/commons/imaging/formats/tiff/TiffReadImageInfoTest.java
+++ src/test/java/org/apache/commons/imaging/formats/tiff/TiffReadImageInfoTest.java
@@ -20,13 +20,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 
+import javax.swing.plaf.synth.ColorType;
+
 import org.apache.commons.imaging.ImageInfo;
 import org.apache.commons.imaging.Imaging;
 import org.apache.commons.imaging.ImagingTestConstants;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
 /**
- * Performs a test on the ImageInfo returned from TIFF to confirm that it contains the specified value for a particular target. This test is used to confirm
+ * Performs a test on the ImageInfo returned from TIFF to confirm that it
+ * contains the specified value for a particular target. This test is used to
+ * confirm
  * that the TiffImageParser is correctly interpreting the ImageInfo values.
  */
 public class TiffReadImageInfoTest extends TiffBaseTest {
@@ -36,8 +41,10 @@ public class TiffReadImageInfoTest extends TiffBaseTest {
     // 1. Parameter field in ImageInfo
     // 2. Expected value
     static final String[][] testSet = { { "1/matthew2.tif", "Color Type", "Black and White" },
-            { "7/Oregon Scientific DS6639 - DSC_0307 - small - CMYK.tiff", "Color Type", "CMYK" }, { "10/Imaging247.TIFF", "Uses Palette", "true" },
-            { "12/TransparencyTestStripAssociated.tif", "Is Transparent", "true" }, { "14/TestJpegStrips.tiff", "Color Type", "YCbCr" } };
+            { "7/Oregon Scientific DS6639 - DSC_0307 - small - CMYK.tiff", "Color Type", "CMYK" },
+            { "10/Imaging247.TIFF", "Uses Palette", "true" },
+            { "12/TransparencyTestStripAssociated.tif", "Is Transparent", "true" },
+            { "14/TestJpegStrips.tiff", "Color Type", "YCbCr" } };
 
     private File getTiffFile(final String name) {
         final File tiffFolder = new File(ImagingTestConstants.TEST_IMAGE_FOLDER, "tiff");
@@ -45,7 +52,8 @@ public class TiffReadImageInfoTest extends TiffBaseTest {
     }
 
     /**
-     * Gets the value for the target data field within the ImageInfo string. This method expects data fields to be given in the form: parameter name, colon,
+     * Gets the value for the target data field within the ImageInfo string. This
+     * method expects data fields to be given in the form: parameter name, colon,
      * value, end-of-line
      *
      * @param info   a valid instance obtained from TiffImageParser
@@ -78,6 +86,50 @@ public class TiffReadImageInfoTest extends TiffBaseTest {
             final String identifier = targetFile.getName() + ": " + testTarget[1];
             assertEquals(value.toLowerCase(), testTarget[2].toLowerCase(), identifier);
         }
+    }
+
+    @Test
+    public void testImageResolution() {
+        ImageInfo mockInfo = mock(ImageInfo.class);
+        when(mockInfo.getPhysicalWidthDpi()).thenReturn(300);
+        when(mockInfo.getPhysicalHeightDpi()).thenReturn(300);
+
+        assertEquals(300, mockInfo.getPhysicalWidthDpi(), "Physical Width DPI mismatch");
+        assertEquals(300, mockInfo.getPhysicalHeightDpi(), "Physical Height DPI mismatch");
+    }
+
+    @Test
+    public void testImageTransparency() {
+        ImageInfo mockInfo = mock(ImageInfo.class);
+        when(mockInfo.isTransparent()).thenReturn(true);
+
+        assertEquals(true, mockInfo.isTransparent(), "Transparency mismatch");
+    }
+
+    @Test
+    public void testBitsPerSample() {
+        ImageInfo mock8Bit = mock(ImageInfo.class);
+        when(mock8Bit.getBitsPerPixel()).thenReturn(8);
+
+        ImageInfo mock16Bit = mock(ImageInfo.class);
+        when(mock16Bit.getBitsPerPixel()).thenReturn(16);
+
+        assertEquals(8, mock8Bit.getBitsPerPixel(), "Bits Per Sample mismatch for 8-bit image");
+        assertEquals(16, mock16Bit.getBitsPerPixel(), "Bits Per Sample mismatch for 16-bit image");
+    }
+
+    @Test
+    public void testCompressionAlgorithm() {
+        ImageInfo mockLZW = mock(ImageInfo.class);
+        when(mockLZW.getCompressionAlgorithm()).thenReturn(ImageInfo.CompressionAlgorithm.LZW);
+
+        ImageInfo mockJPEG = mock(ImageInfo.class);
+        when(mockJPEG.getCompressionAlgorithm()).thenReturn(ImageInfo.CompressionAlgorithm.JPEG);
+
+        assertEquals(ImageInfo.CompressionAlgorithm.LZW, mockLZW.getCompressionAlgorithm(),
+                "Compression Algorithm mismatch for LZW image");
+        assertEquals(ImageInfo.CompressionAlgorithm.JPEG, mockJPEG.getCompressionAlgorithm(),
+                "Compression Algorithm mismatch for JPEG image");
     }
 
 }
